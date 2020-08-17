e1290fd31b3068940ebf04cb78731e824ef8d230
FreddyJR1995 <56648676+FreddyJR1995@users.noreply.github.com>
2020-08-16 22:08:07 -0500

    #6 Seeders de Notification y Category

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Vaciar la tabla.
+        Category::truncate();
+
+        $faker = \Faker\Factory::create();
+        // Crear productos ficticios en la table
+
+        for($i = 0; $i < 20; $i++) {
+            Category::create([
+                'name'=> $faker->name,
+            ]);
+        }
+    }
+}
