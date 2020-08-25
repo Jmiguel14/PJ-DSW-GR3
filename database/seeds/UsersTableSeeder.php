c8fd63f5c6093921e361b8ab84756ab70a0151d3
Jmiguel14 <miguel.jurado@epn.edu.ec>
2020-08-25 10:13:01 -0500

    Auth-Jwt
    Creación de Cors
    
    Creación de Seeder y edición de migraciones de User

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        $faker = \Faker\Factory::create();
+        // Crear la misma clave para todos los usuarios
+        // conviene hacerlo antes del for para que el seeder
+        // no se vuelva lento.
+        $password = Hash::make('123456');
+        User::create([
+            'name' => 'Administrador',
+            'lastname' => 'Prueba',
+            'email' => 'admin@prueba.com',
+            'password' => $password,
+            'phone' => 988185518,
+            'business_name' => 'tienda carmita',
+            'description' => 'Mini marquet',
+        ]);
+        // Generar algunos usuarios para nuestra aplicacion
+        for ($i = 0; $i < 9; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'lastname' => $faker->lastName,
+                'email' => $faker->email,
+                'password' => $password,
+                'phone' => $faker->phoneNumber,
+                'business_name' => $faker->company,
+                'description' => $faker->paragraph,
+            ]);
+        }
+    }
+}
